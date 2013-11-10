51316aaf32e9c1194aabf289479654ea7dbca59f
ygarbage <user@server.realfake>
2013-11-10 19:38:26 +0100

    Correction de la 4ème requête de statistique. Elle affiche maintenant tous les idCommentaire selon leur indice de confiance, même ceux qui n'ont eu aucun pouces.

--- mysql_test/stats4.sql
+++ mysql_test/stats4.sql
@@ -1,13 +1,13 @@
 -- Calcul de l'indice de confiance, avec les détails du calcul
 SELECT idCommentaire, (1 + res.c) as "1+c", (1 + res.d) as "1+d", (1 + res.c)/(1 + res.d) as "Indice de confiance" FROM (
        SELECT commentaire.idCommentaire, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
-       FROM pouce INNER JOIN commentaire ON pouce.idCommentaire = commentaire.idCommentaire
-       GROUP BY commentaire.idCommentaire) as res
-ORDER BY (1 + res.c)/(1 + res.d) DESC;
+       FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
+       GROUP BY commentaire.idCommentaire) AS res
+       ORDER BY (1 + res.c)/(1 + res.d) DESC;
 
--- Les idCommentaire selon leur indice de confiance
+-- -- Les idCommentaire selon leur indice de confiance
 SELECT idCommentaire, (1 + res.c)/(1 + res.d) as "Indice de confiance" FROM (
        SELECT commentaire.idCommentaire, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
-       FROM pouce INNER JOIN commentaire ON pouce.idCommentaire = commentaire.idCommentaire
-       GROUP BY commentaire.idCommentaire) as res
+       FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
+       GROUP BY commentaire.idCommentaire) AS res
        ORDER BY (1 + res.c)/(1 + res.d) DESC;
