d891d6e7208e3e6c16bbb0f4730ea1536782f112
plefebvre91 <plefebvre@enseirb-matmeca.fr>
2013-11-21 15:34:51 +0100

    Collaspse des formulaire MAJ apres modification

--- site/js/update.js
+++ site/js/update.js
@@ -1,9 +1,11 @@
 function update_player(pseudo){
     var resultId = "#result-"+pseudo;
     $(resultId).html(pseudo + " - Chargement...");
+    var collapseId = "#collapse-"+pseudo;    
     var formId = "#form-maj-"+pseudo;
     var request = $.get("ajax/ajax_update_player.php", $(formId).serialize()+"&pseudo="+pseudo);
-    request.done(function(msg){$(resultId).html(pseudo + " - " + msg);});
+    request.done(function(msg){$(resultId).html(pseudo + " - " + msg);
+			       $(collapseId).fadeOut("slow");
+    $(collapseId).toggleClass("panel-collapse collapse");});
     request.fail(failure);
-    return false;
 }
