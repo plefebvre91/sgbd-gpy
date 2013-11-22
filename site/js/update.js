ccc89fe93490a610008b0f37e2222ce13c2c2a8a
Grégoire <gregoire@gregoire-Latitude-E5530-non-vPro.(none)>
2013-11-22 13:41:18 +0100

    Modif js pour up

--- site/js/update.js
+++ site/js/update.js
@@ -9,3 +9,15 @@ function update_player(pseudo){
     $(collapseId).toggleClass("panel-collapse collapse");});
     request.fail(failure);
 }
+
+function update_comment(idCommentaire){
+    var resultId = "#result-"+idCommentaire;
+    $(resultId).html(idCommentaire + " - Chargement...");
+    var collapseId = "#collapse-"+idCommentaire;    
+    var formId = "#form-maj-"+idCommentaire;
+    var request = $.get("ajax/ajax_update_comment.php", $(formId).serialize());
+    request.done(function(msg){$(resultId).html(idCommentaire + " - " + msg);
+			       $(collapseId).fadeOut("slow");
+    $(collapseId).toggleClass("panel-collapse collapse");});
+    request.fail(failure);
+}
