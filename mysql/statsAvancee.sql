fb3e4a54a7376a6d95d85db2beb630e61b9356e1
ygarbage <ygarbage@enseirb-matmeca.fr>
2013-12-01 17:00:56 +0100

    Amélioration de la stats avancée : affichage du nom du jeu à la place de l'idJeu. Si 2 jeux ont une moyenne pondérée identique, celui dont la somme des Indices de Confiance des commentaires est la plus élevée est mieux classé.

--- mysql/statsAvancee.sql
+++ mysql/statsAvancee.sql
@@ -1,25 +1,16 @@
--- Classement des idJeu COMMENTES AU MOINS UNE FOIS par moyenne ponderee decroissante
+-- Classement des jeux COMMENTES AU MOINS UNE FOIS par moyenne ponderee decroissante puis par somme des IC des commentaires décroissante
+SELECT nomJeu as "Nom du jeu", moyennePonderee as "Moyenne pondérée" FROM jeu NATURAL JOIN (
+SELECT idJeu, ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) AS "moyennePonderee" FROM (
+       SELECT commentaire.*, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
+       FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
+       GROUP BY commentaire.idCommentaire) AS res
+       GROUP BY idJeu
+       ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC, sum((1 + res.c)/(1 + res.d)) DESC) as classement;
+
+-- Classement des idJeu COMMENTES AU MOINS UNE FOIS par moyenne ponderee decroissante puis par somme des IC des commentaires décroissante
 SELECT idJeu, ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) AS "Moyenne ponderee" FROM (
        SELECT commentaire.*, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
        FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
        GROUP BY commentaire.idCommentaire) AS res
        GROUP BY idJeu
-       ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC;
-
--- Pour chaque jeu : somme des IC, somme des notes ponderees, et moyenne ponderee
--- SELECT idJeu, sum((1 + res.c)/(1 + res.d)) AS "Somme IC", sum(note  * (1 + res.c)/(1 + res.d)) AS "Somme notes ponderees",
--- ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) AS "Moyenne ponderee" FROM (
---        SELECT commentaire.*, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
---        FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
---        GROUP BY commentaire.idCommentaire) AS res
---        GROUP BY idJeu
---        ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC;
-
--- -- Les idJeu et le total des notes non ponderees de chaque jeu, classe par idJeu
--- SELECT idJeu, sum(note) AS "Total notes" FROM (
--- SELECT idCommentaire, idJeu, note, (1 + res.c)/(1 + res.d) AS "Indice de confiance", note  * (1 + res.c)/(1 + res.d) AS "Note ponderee" FROM (
---        SELECT commentaire.*, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
---        FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
---        GROUP BY commentaire.idCommentaire) AS res
---        ORDER BY idJeu ASC) as result1
---        GROUP BY idJeu;
+       ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC, sum((1 + res.c)/(1 + res.d)) DESC;
