ed9fd84bccfc9e69e7fbbad6458bddca3b0c361a
ygarbage <ygarbage@enseirb-matmeca.fr>
2013-12-01 17:07:56 +0100

    Ajout de l'affichage de la somme des Indices de Confiance pour montrer le traitement en cas d'égalité dans la requête de statistique avancée. Mise en majuscule des mots clés.

--- mysql/statsAvancee.sql
+++ mysql/statsAvancee.sql
@@ -1,16 +1,8 @@
 -- Classement des jeux COMMENTES AU MOINS UNE FOIS par moyenne ponderee decroissante puis par somme des IC des commentaires décroissante
-SELECT nomJeu as "Nom du jeu", moyennePonderee as "Moyenne pondérée" FROM jeu NATURAL JOIN (
-SELECT idJeu, ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) AS "moyennePonderee" FROM (
+SELECT nomJeu AS "Nom du jeu", MP AS "Moyenne pondérée", TotalIC AS "Total des Indices de Confiance des commentaires du jeu" FROM jeu NATURAL JOIN (
+SELECT idJeu, ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) AS "MP", sum((1 + res.c)/(1 + res.d)) AS "TotalIC" FROM (
        SELECT commentaire.*, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
        FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
        GROUP BY commentaire.idCommentaire) AS res
        GROUP BY idJeu
-       ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC, sum((1 + res.c)/(1 + res.d)) DESC) as classement;
-
--- Classement des idJeu COMMENTES AU MOINS UNE FOIS par moyenne ponderee decroissante puis par somme des IC des commentaires décroissante
-SELECT idJeu, ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) AS "Moyenne ponderee" FROM (
-       SELECT commentaire.*, SUM(IF(pouce.valeur = '+', 1, 0)) AS "c", SUM(IF(pouce.valeur = '-', 1, 0)) AS "d"
-       FROM commentaire LEFT OUTER JOIN pouce ON commentaire.idCommentaire = pouce.idCommentaire
-       GROUP BY commentaire.idCommentaire) AS res
-       GROUP BY idJeu
-       ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC, sum((1 + res.c)/(1 + res.d)) DESC;
+       ORDER BY ( sum(note  * (1 + res.c)/(1 + res.d)) ) / ( sum((1 + res.c)/(1 + res.d)) ) DESC, sum((1 + res.c)/(1 + res.d)) DESC) AS classement;
