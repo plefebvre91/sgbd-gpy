0fa38981ecc8594065c9e1a2f5b8d3874bae15c3
ygarbage <ygarbage@enseirb-matmeca.fr>
2013-12-03 22:24:55 +0100

    Dans la MAJ d'un jeu, ajout de la possibilité de ne pas ajouter de plateforme ou de ne pas ajouter de catégorie. IL ME SEMBLE avoir vu un bug en testant, mais je n'ai pas réussi à le reproduire. Pouvez-vous tester un peu ça ?

--- site/ajax/ajax_update_game.php
+++ site/ajax/ajax_update_game.php
@@ -14,9 +14,24 @@ if(!$err){
 $game_name = secure_string($_GET["nomJeu"]);
 $id_editor = secure_string($_GET["idEditeur"]);
 $game_id = secure_string($_GET["idJeu"]);
-$categories = $_GET["idCategorie"];
-$platforms = $_GET["idPlateforme"];
-$dates = $_GET["dateSortie"];
+
+// Traitement différent selon ce que l'utilisateur a fourni
+// Si aucune case plateforme n'est cochée
+if ( ! isset($_GET["idPlateforme"]) ) {
+  $platforms = NULL;
+}
+else {
+  $platforms = $_GET["idPlateforme"];
+}
+// Si aucune case catégorie n'est cochée
+if ( !isset($_GET["idCategorie"]) ) {
+  $categories = NULL;
+}
+else {
+  $categories = $_GET["idCategorie"];
+}
+
+$dates = $_GET["dateSortie"]; // Ne cause pas d'erreur
 
 //Selection dans la base
 if(!update_game($game_id, $game_name, $id_editor, $categories, $platforms, $dates)) {
