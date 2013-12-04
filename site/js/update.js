7b9b5e2a3f470e352ab5f69ea0e8d84998049c5b
ygarbage <ygarbage@enseirb-matmeca.fr>
2013-12-04 13:21:57 +0100

    Vérification que tous les champs sont corrects dans la MAJ.

--- site/js/update.js
+++ site/js/update.js
@@ -4,10 +4,10 @@ function update_player(pseudo){
     var formId = "#form-maj-"+pseudo;
     var url = $(formId).serialize();
     if (url.indexOf("=&") != -1) {
-	$(resultId).html(pseudo + " - Veuillez remplir tous les champs.");
+	$(resultId).html(pseudo + " - Veuillez remplir tous les champs correctement.");
 	return;
     }
-    var request = $.get("ajax/ajax_update_player.php", url + "&pseudo="+pseudo);
+    var request = $.get("ajax/ajax_update_player.php", url + "&pseudo=" + pseudo);
     request.done(function(msg){$(resultId).html(pseudo + " - " + msg);
 			       $(resultId).collapse("hide");});
     request.fail(failure);
@@ -17,7 +17,12 @@ function update_game(game_id){
     var resultId = "#result-jeu-"+game_id;
     $(resultId).html(" - Chargement...");
     var formId = "#form-maj-jeu-"+game_id;
-    var request = $.get("ajax/ajax_update_game.php", $(formId).serialize()+"&idJeu="+game_id);
+    var url = $(formId).serialize();
+    if (url.indexOf("=&idEditeur") != -1) {
+	$(resultId).html("Veuillez saisir le nom du jeu.");
+	return;
+    }
+    var request = $.get("ajax/ajax_update_game.php", url + "&idJeu=" + game_id);
     request.done(function(msg){$(resultId).html(msg);
 			       $(resultId).collapse("hide");});
     request.fail(failure);
@@ -28,7 +33,12 @@ function update_comment(idCommentaire){
     var resultId = "#result-commentaire-"+idCommentaire;
     $(resultId).html(idCommentaire + " - Chargement...");
     var formId = "#form-maj-commentaire-"+idCommentaire;
-    var request = $.get("ajax/ajax_update_comment.php", $(formId).serialize()+"&idCommentaire="+idCommentaire);
+    var url = $(formId).serialize();
+    if (url.indexOf("=&") != -1) {
+	$(resultId).html("Commentaire " + idCommentaire + " - Veuillez remplir tous les champs correctement.");
+	return;
+    }
+    var request = $.get("ajax/ajax_update_comment.php", url + "&idCommentaire=" + idCommentaire);
     request.done(function(msg){$(resultId).html("Commentaire "+ idCommentaire + " - " + msg);
 			       $(resultId).collapse("hide");});
     request.fail(failure);
