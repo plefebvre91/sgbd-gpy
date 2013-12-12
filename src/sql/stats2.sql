f8e6e72a3368ccb1a25289424646ad8b05375b87
plefebvre91 <plefebvre@enseirb-matmeca.fr>
2013-12-12 20:46:06 +0100

    Effacement des anciens fichiers

--- src/sql/stats2.sql
+++
@@ -1,4 +0,0 @@
--- La liste des n commentaires les plus recents
-SELECT * FROM commentaire
-ORDER BY commentaire.dateCommentaire DESC
-LIMIT 4; -- Todo : 4 a remplacer par le parametre n
--- src/sql/stats3.sql
+++
@@ -1,5 +0,0 @@
--- le commentaire qui laisse le moins indifférent (celui qui a reçu le plus de jugements) ;
-SELECT distinct commentaire.* FROM pouce INNER JOIN commentaire ON pouce.idCommentaire = commentaire.idCommentaire
-       where pouce.idCommentaire in
-       (SELECT idCommentaire FROM pouce GROUP BY pouce.idCommentaire HAVING count(*) = (SELECT count(*) FROM pouce GROUP BY pouce.idCommentaire ASC LIMIT 1));
-       
